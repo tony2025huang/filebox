8ddd6f5233be555e444b66c46bfbb9c650b0e3ed
dsh&codex <tony2025huang@users.noreply.github.com>
2026-09-01 10:24:29 -0700

    v017 需求3: 导航顺序调整(我的文件/我的收集/我的分享/同步任务/日志/系统设置) + nav键三语更新

--- web/src/components/AuthenticatedTopbar.vue
+++ web/src/components/AuthenticatedTopbar.vue
@@ -5,11 +5,11 @@
       <slot name="actions" />
       <LanguageSelect :user="user" />
       <RouterLink to="/" class="icon-text-button"><FolderOpen :size="16" /> {{ t('nav.files') }}</RouterLink>
-      <RouterLink to="/shares" class="icon-text-button"><Share2 :size="16" /> {{ t('nav.shares') }}</RouterLink>
-      <RouterLink to="/sync" class="icon-text-button"><RefreshCw :size="16" /> {{ t('nav.sync') }}</RouterLink>
-      <RouterLink to="/logs" class="icon-text-button"><ScrollText :size="16" /> {{ t('nav.logs') }}</RouterLink>
       <RouterLink to="/collections" class="icon-text-button"><UploadCloud :size="16" /> {{ t('nav.collections') }}</RouterLink>
-      <RouterLink v-if="user.role === 'admin'" to="/admin" class="icon-text-button"><Shield :size="16" /> {{ t('nav.admin') }}</RouterLink>
+      <RouterLink to="/shares" class="icon-text-button"><Share2 :size="16" /> {{ t('nav.shares') }}</RouterLink>
+      <RouterLink to="/sync" class="icon-text-button"><RefreshCw :size="16" /> {{ t('nav.syncTasks') }}</RouterLink>
+      <RouterLink to="/logs" class="icon-text-button"><ScrollText :size="16" /> {{ t('nav.logs') }}</RouterLink>
+      <RouterLink v-if="user.role === 'admin'" to="/admin" class="icon-text-button"><Shield :size="16" /> {{ t('nav.system') }}</RouterLink>
       <RouterLink to="/change-password?mode=self" class="icon-button" :title="t('nav.changePassword')"><KeyRound :size="17" /></RouterLink>
       <button class="icon-button" :title="t('nav.logout')" @click="logout"><LogOut :size="18" /></button>
     </div>
